ba90cdad4e7145c0d9cec416ea02f640270aa155
Raffaella <pivetta.raffaella@gmail.com>
2017-12-12 14:09:43 +0100

    inserita la classe per l'oggetto BonusPoint

---
+++ app/src/main/java/com/valeriamauro/raffaellapivetta/mypointdiet/MyPointDiet_Object/BonusPoint.java
@@ -0,0 +1,38 @@
+package com.valeriamauro.raffaellapivetta.mypointdiet.MyPointDiet_Object;
+
+import com.valeriamauro.raffaellapivetta.mypointdiet._MyPointDiet.MyBonusPoint;
+
+import java.util.Date;
+
+
+public class BonusPoint implements MyBonusPoint {
+    @Override
+    public String getKeyBonusPoint() {
+        return null;
+    }
+
+    @Override
+    public Date getLastUpdate() {
+        return null;
+    }
+
+    @Override
+    public Float getPointSaved() {
+        return null;
+    }
+
+    @Override
+    public void setKeyBonusPoint(String bonusPoint) {
+
+    }
+
+    @Override
+    public void setMyLastUpdate(Date lastUpdate) {
+
+    }
+
+    @Override
+    public void setPointSaved(Float pointSaved) {
+
+    }
+}
