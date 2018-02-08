4fe946b84b9b685be2e838e094fc752eba3641c9
Raffaella.Pivetta <pivetta.raffaellagmail.com>
2018-02-08 18:05:50 +0100

    aggiunti gli attributi della classe

--- app/src/main/java/com/valeriamauro/raffaellapivetta/mypointdiet/MyPointDiet_Object/FoodDay.java
+++ app/src/main/java/com/valeriamauro/raffaellapivetta/mypointdiet/MyPointDiet_Object/FoodDay.java
@@ -8,64 +8,70 @@ import com.valeriamauro.raffaellapivetta.mypointdiet._MyPointDiet.MyFoodDay;
 
 public class FoodDay implements MyFoodDay {
 
+    private String keyFoodDay;
+    private String nameFoodDay;
+    private Float quantityFoodDay;
+    private String misureFoodDay;
+    private String typeFoodDay;
+    private Float pointFoodDay;
 
     @Override
     public String getKeyFoodDay() {
-        return null;
+        return keyFoodDay;
     }
 
     @Override
     public String getNameFoodDay() {
-        return null;
+        return nameFoodDay;
     }
 
     @Override
     public Float getQuantityFoodDay() {
-        return null;
+        return quantityFoodDay;
     }
 
     @Override
     public String getMisureFoodDay() {
-        return null;
+        return misureFoodDay;
     }
 
     @Override
     public String getTypeFoodDay() {
-        return null;
+        return typeFoodDay;
     }
 
     @Override
     public Float getPointFoodDay() {
-        return null;
+        return pointFoodDay;
     }
 
     @Override
     public void setKeyFoodDay(String keyFoodDay) {
-
+        this.keyFoodDay=keyFoodDay;
     }
 
     @Override
     public void setNameFoodDay(String nameFoodDay) {
-
+        this.nameFoodDay=nameFoodDay;
     }
 
     @Override
     public void setQuantityFoodDay(Float quantityFoodDay) {
-
+        this.quantityFoodDay=quantityFoodDay;
     }
 
     @Override
     public void setMisureFoodDay(String misureFoodDay) {
-
+        this.misureFoodDay=misureFoodDay;
     }
 
     @Override
     public void setTypeFoodDay(String typeFoodDay) {
-
+        this.typeFoodDay=typeFoodDay;
     }
 
     @Override
     public void setPointFoodDay(Float pointFoodDay) {
-
+        this.pointFoodDay=pointFoodDay;
     }
 }
