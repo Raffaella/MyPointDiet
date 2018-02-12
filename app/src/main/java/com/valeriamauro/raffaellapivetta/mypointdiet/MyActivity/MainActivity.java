0dcc6c6dea08c65ffbe18a82e2a212ace4838df7
Raffaella.Pivetta <pivetta.raffaellagmail.com>
2018-02-12 15:19:38 +0100

    inserimento main activity

---
+++ app/src/main/java/com/valeriamauro/raffaellapivetta/mypointdiet/MyActivity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.valeriamauro.raffaellapivetta.mypointdiet.MyActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.valeriamauro.raffaellapivetta.mypointdiet.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
