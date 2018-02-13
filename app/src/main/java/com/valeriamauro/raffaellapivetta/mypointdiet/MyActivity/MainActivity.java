3a349db3136c5440a5260db9ef313fc0e0d6dcc3
Raffaella.Pivetta <pivetta.raffaellagmail.com>
2018-02-13 17:32:19 +0100

    inserita la BaseActivity

--- app/src/main/java/com/valeriamauro/raffaellapivetta/mypointdiet/MyActivity/MainActivity.java
+++ app/src/main/java/com/valeriamauro/raffaellapivetta/mypointdiet/MyActivity/MainActivity.java
@@ -3,9 +3,10 @@ package com.valeriamauro.raffaellapivetta.mypointdiet.MyActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.valeriamauro.raffaellapivetta.mypointdiet.MyPointDiet_Base.BaseActivity;
 import com.valeriamauro.raffaellapivetta.mypointdiet.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
